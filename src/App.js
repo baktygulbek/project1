fcfc18e6b301c94eb1ccf87bddf54417809e2063
baktygulbek <bekkebaktygul@gmail.com>
2023-07-18 14:10:59 +0600

    forst commint

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from './components/ui/Button';
+import Card from './components/ui/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button/>
+      <Card>baktygul</Card>
+    </div>
+  );
+}
+
+export default App;
+ 
